8eb65cb7898e2206fe8e04aa5849191edb6bf0ff
Mayank Mishra <mayankmishra3036@gmail.com>
2026-04-10 02:07:57 +0530

    fix port for render

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ app.delete("/tasks/:id", (req, res) => {
   res.json({ message: "Task deleted" });
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
