acbd9cec1994a62c8a0731fd563ff575ebd7e27e
Mayank Mishra <mayankmishra3036@gmail.com>
2026-04-10 02:47:23 +0530

    fix api url for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import TaskItem from "./components/TaskItem";
 import FilterBar from "./components/FilterBar";
 import Stats from "./components/Stats";
 
-const API = "https://task-manager-fullstack-yjub.onrender.com/";
+const API = "https://task-manager-fullstack-yjub.onrender.com";
 
 function App() {
   const [tasks, setTasks] = useState([]);
